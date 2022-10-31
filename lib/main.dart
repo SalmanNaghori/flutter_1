7ba963e91445879346787ab9bc53f8becb2d7b6c
SalmanNaghori <salmannaghori@gmail.com>
2022-10-31 19:53:46 +0530

    theme extracted

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_1/pages/home_page.dart';
 import 'package:flutter_1/pages/login_page.dart';
 import 'package:flutter_1/util/routes.dart';
+import 'package:flutter_1/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_1/util/routes.dart';
 
@@ -16,12 +17,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
-      darkTheme: ThemeData(primarySwatch: Colors.red),
-
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       routes: {
         "/": (context) => HomePage(),
         MyRoutes.homeRoute: (context) => HomePage(),
