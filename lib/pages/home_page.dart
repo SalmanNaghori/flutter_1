4d2fca18e8ef7978f97bcb42ee6bd7d552cec946
SalmanNaghori <salmannaghori@gmail.com>
2023-01-03 13:18:58 +0530

    day11

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_1/models/catalog.dart';
 import 'package:flutter_1/widgets/drawer.dart';
 import 'package:flutter_1/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "Jarvis";
   //day 10 learned about context , constraints
+  @override
+  void initState() {
+    super.initState();
+    loaddata();
+  }
+
+  loaddata() async {
+    var catalogjson = rootBundle.loadString("assets/file/catalog.json");
+    var decodedata = jsonDecode("assets/file/catalog.json");
+    print(decodedata);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +39,10 @@ class HomePage extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
           itemCount: CalatogModel.item.length,
-          itemBuilder: (context,index){
-          return ItemWidget(item:CalatogModel.item[index],);
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: CalatogModel.item[index],
+            );
           },
         ),
       ),
